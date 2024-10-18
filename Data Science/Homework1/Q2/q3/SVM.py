c4c3f1fdccf41faf7f9a6a5c9ef8885f1e04c17c
xunying123 <xun_ying123@163.com>
2024-10-18 14:05:45 +0800

    456789076546

--- Data Science/Homework1/Q2/q3/SVM.py
+++ Data Science/Homework1/Q2/q3/SVM.py
@@ -8,11 +8,11 @@ train_label = np.load('./Data Science/Homework1/Q2/dataset/3/train_label.npy')
 test_data = np.load('./Data Science/Homework1/Q2/dataset/3/test_data.npy')
 test_label = np.load('./Data Science/Homework1/Q2/dataset/3/test_label.npy')
 
-# 假设已经有合并后的数据 combined_data 和 combined_label
+#合并后的数据 combined_data 和 combined_label
 X_train, X_test, y_train, y_test = train_test_split(train_data, train_label, test_size=0.2, random_state=42)
 
 # SVM 模型
-svm_model = SVC(kernel='linear')  # 使用线性核，也可以尝试 'rbf'
+svm_model = SVC(kernel='linear') 
 svm_model.fit(X_train, y_train)
 y_pred_svm = svm_model.predict(X_test)
 svm_accuracy = accuracy_score(y_test, y_pred_svm)
